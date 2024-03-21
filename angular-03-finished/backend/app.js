500f16af3e5ecf5465bcda26ba7de69dfca4f39d
mucacran <el.mucacran.rasta@hotmail.com>
2024-03-21 16:58:55 -0500

    listo

--- angular-03-finished/backend/app.js
+++ angular-03-finished/backend/app.js
@@ -2,13 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Este es el primer intermediario");
-    next();
+app.use('/api/posts',(req, res, next) => {
+    //console.log("Este es el primer intermediario");
+    //next();
+    const posts = [
+{
+    id: "pepepepe",
+    title: "First server-side post",
+    content: "This is coming grom Serfe"
+},{
+    id: "fskdhfksj",
+    title: "Sgundo server-side post",
+    content: "This is coming from Serfe"
+}
+    ];
+    res.status(200).json({
+        message: 'Posts fetche satisfactoriamente!',
+        posts: posts
+    });
 });
 
+/*
 app.use((req, res, next) => {
     res.send('Hola panchito lo hiciste super bien');
-});
+});*/
 
 module.exports = app;

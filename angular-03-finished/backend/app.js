925f87da1132fecbddb2c261342c6dd2510585dd
mucacran <el.mucacran.rasta@hotmail.com>
2024-03-31 19:01:39 -0500

    actualizao
    pero me sale nulo, voy a reemplazarlo por el archivo que me dio el tutor

--- angular-03-finished/backend/app.js
+++ angular-03-finished/backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bP = require('body-parser');
 
+const Post = require('./models/post');
+
 const app = express();
 
 app.use(bP.json());
-app.use(bP.urlencoded({extended:false}));
+app.use(bP.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,8 +15,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/posts',(req, res, next)=>{
-    const post = req.body;
+app.post('/api/posts', (req, res, next) => {
+    //const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+
     console.log(req);
     res.status(201).json({
         message: 'Post añadido satisfactoriamente'
@@ -22,19 +29,19 @@ app.post('/api/posts',(req, res, next)=>{
 });
 
 
-app.use('/api/posts',(req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
     //console.log("Este es el primer intermediario");
     //next();
     const posts = [
-{
-    id: "pepepepe",
-    title: "First server-side post",
-    content: "This is coming grom Serfe"
-},{
-    id: "fskdhfksj",
-    title: "Sgundo server-side post",
-    content: "This is coming from Serfe"
-}
+        {
+            id: "pepepepe",
+            title: "First server-side post",
+            content: "This is coming grom Serfe"
+        }, {
+            id: "fskdhfksj",
+            title: "Sgundo server-side post",
+            content: "This is coming from Serfe"
+        }
     ];
     res.status(200).json({
         message: 'Posts fetche satisfactoriamente!',

79026a121b77ab9086a1ccd6878a6d08920b417f
mucacran <el.mucacran.rasta@hotmail.com>
2024-03-31 20:01:18 -0500

    este es el archivo del profesor
    no se como pero aqui si se resuelve

--- angular-03-finished/backend/app.js
+++ angular-03-finished/backend/app.js
@@ -1,57 +1,54 @@
-const express = require('express');
-const bP = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const Post = require('./models/post');
 
 const app = express();
 
-app.use(bP.json());
-app.use(bP.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+// QuBqs0T45GDKPlIG
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
-app.post('/api/posts', (req, res, next) => {
-    //const post = req.body;
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-
-    console.log(req);
-    res.status(201).json({
-        message: 'Post añadido satisfactoriamente'
-    });
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "fadf12421l",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  });
 });
-
-
-app.get('/api/posts', (req, res, next) => {
-    //console.log("Este es el primer intermediario");
-    //next();
-    const posts = [
-        {
-            id: "pepepepe",
-            title: "First server-side post",
-            content: "This is coming grom Serfe"
-        }, {
-            id: "fskdhfksj",
-            title: "Sgundo server-side post",
-            content: "This is coming from Serfe"
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetche satisfactoriamente!',
-        posts: posts
-    });
-});
-
-/*
-app.use((req, res, next) => {
-    res.send('Hola panchito lo hiciste super bien');
-});*/
 
 module.exports = app;
